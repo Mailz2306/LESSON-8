13cc7359c580540695a03d82cadc7bdad746197f
mailz <sergey4istyak@yandex.ru>
2023-04-16 13:46:27 +0400

    решена задача 1

--- Program.cs
+++ Program.cs
@@ -156,7 +156,7 @@ void ChangeRowsArray(int[,] array)
 3 встречается 1 раз
 4 встречается 1 раз
 6 встречается 2 раза
-*/
+
 
 int[,] array = Generate2DArray(2, 15);
 Console.WriteLine("Вот твой массив, кожаный ублюдок");
@@ -219,7 +219,84 @@ Dictionary<int, int> SummIndexTimes(int[,] array)
     }
     return dic;
 }
+_______________________
+КОНЕЦ СЕМИНАРА 
+ДОМАШНИЕ ЗАДАНИЯ 
+Задача 54:
+Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию 
+элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+РЕШЕНИЕ: 
+___________________________
+Console.Write("Введите размерность m массива: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите размерность n массива: ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] randomArray = new int[m, n];
+
+void GenerateArray(int m, int n)
+{
+    int i, j;
+    Random rand = new Random();
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            randomArray[i, j] = rand.Next(1, 9);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    int i, j;
+    for (i = 0; i < array.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+    }
+}
+
+GenerateArray(m, n);
+Console.WriteLine("Например, задан массив: ");
+PrintArray(randomArray);
+
+void DecrieseArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+DecrieseArray(randomArray);
+Console.WriteLine();
+Console.WriteLine("В итоге получается вот такой массив: ");
+PrintArray(randomArray);
+____________________________ 
 
 
-
+*/
 
