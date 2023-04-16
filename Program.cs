e1715304cdada37cbfb1ee7a08b1e08a49b064af
mailz <sergey4istyak@yandex.ru>
2023-04-16 13:24:34 +0400

    конец занятия

--- Program.cs
+++ Program.cs
@@ -75,13 +75,17 @@ ___________
 Задайте двумерный массив. Напишите программу, которая 
 заменяет строки на столбцы. В случае, если это невозможно, программа 
 должна вывести сообщение для пользователя.
-*/
 
 int[,] array = Generate2DArray(3, 3);
 
+RevesreArray(array);
+
 Print2DArray(array);
+
 ChangeRowsArray(array);
+
 Console.WriteLine();
+
 Print2DArray(array);
 
 int[,] Generate2DArray(int m, int n)
@@ -106,13 +110,13 @@ void Print2DArray(int[,] array)
             Console.Write(array[i, j]);
             Console.Write(" ");
         }
-
         Console.WriteLine();
     }
 }
-/*
+
 void RevesreArray(int[,] array)
 {
+
     for (int i = 0; i < array.GetLength(1); i++)
     {
         int tmp = array[0, i];
@@ -120,24 +124,102 @@ void RevesreArray(int[,] array)
         array[array.GetLength(0) - 1, i] = tmp;
     }
 }
-*/ 
+
 void ChangeRowsArray(int[,] array)
 {
-/*array[0, 0]
-array[0, 0]
-array[0, 0]
-array[0, 0]
-
-array[0, 0]
-array[0, 0]
-array[0, 0]
-array[0, 0]
-*/
-
-    for (int i = 0; i < array.GetLength(1); i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        int tmp = array[0, i];
-        array[0, i] = array[array.GetLength(0) - 1, i];
-        array[array.GetLength(0) - 1, i] = tmp;
+        for (int j = i; j < array.GetLength(1); j++)
+        {
+            int tmp = array[i, j];
+            array[i, j] = array[j, i];
+            array[j, i] = tmp;
+        }
     }
 }
+
+ЗАДАЧА 57 
+Составить частотный словарь элементов двумерного массива. 
+Частотный словарь содержит информацию о том, сколько раз встречается 
+элемент входных данных.
+Набор данных
+Частотный массив
+{ 1, 9, 9, 0, 2, 8, 0, 9 }
+0 встречается 2 раза
+1 встречается 1 раз
+2 встречается 1 раз
+8 встречается 1 раз
+9 встречается 3 раза
+1, 2, 3 4, 6, 1 2, 1, 6
+1 встречается 3 раза
+2 встречается 2 раз
+3 встречается 1 раз
+4 встречается 1 раз
+6 встречается 2 раза
+*/
+
+int[,] array = Generate2DArray(2, 15);
+Console.WriteLine("Вот твой массив, кожаный ублюдок");
+PrintArray(array);
+Console.WriteLine("В этом массиве: ");
+Dictionary<int, int> result = SummIndexTimes(array);
+foreach(var row in result)
+{
+    if (row.Value == (1) | row.Value == (5)| row.Value == (6))
+    {
+        Console.WriteLine($"Цифра {row.Key} встречается {row.Value} раз");
+    } 
+        else 
+        {
+            Console.WriteLine($"Цифра {row.Key} встречается {row.Value} раза");
+        }
+}
+void PrintArray(int[,] array)
+{
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j]);
+            Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Generate2DArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+
+    Random random = new Random();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = random.Next(1, 10);
+        }
+    }
+
+    return array;
+}
+
+Dictionary<int, int> SummIndexTimes(int[,] array)
+{
+    Dictionary<int, int> dic = new Dictionary<int, int>();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (!dic.TryAdd(array[i, j], 1))
+            {
+                dic[array[i, j]] += 1;
+            }
+        }
+    }
+    return dic;
+}
+
+
+
+
