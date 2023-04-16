1e11a729f0addd7764408566783f386d94ac92ca
mailz <sergey4istyak@yandex.ru>
2023-04-16 11:39:05 +0400

    решаем задачи

---
+++ Program.cs
@@ -0,0 +1,143 @@
+﻿/*
+Задача 53: Задайте двумерный массив. Напишите программу, которая 
+поменяет местами первую и последнюю строку массива.
+Задача 55: Задайте двумерный массив. Напишите программу, которая 
+заменяет строки на столбцы. В случае, если это невозможно, программа 
+должна вывести сообщение для пользователя.
+Задача 57: Составить частотный словарь элементов двумерного массива. 
+Частотный словарь содержит информацию о том, сколько раз встречается 
+элемент входных данных.
+Набор данных
+Частотный массив
+{ 1, 9, 9, 0, 2, 8, 0, 9 }
+0 встречается 2 раза
+1 встречается 1 раз
+2 встречается 1 раз
+8 встречается 1 раз
+9 встречается 3 раза
+1, 2, 3 4, 6, 1 2, 1, 6
+1 встречается 3 раза
+2 встречается 2 раз
+3 встречается 1 раз
+4 встречается 1 раз
+6 встречается 2 раза
+
+ЗАДАЧА 53
+
+int[,] array = Generate2DArray(3, 4);
+
+Print2DArray(array);
+RevesreArray(array);
+Console.WriteLine();
+Print2DArray(array);
+
+int[,] Generate2DArray(int m, int n)
+{
+
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j]);
+            Console.Write(" ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+void RevesreArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        int tmp = array[0, i];
+        array[0, i] = array[array.GetLength(0) - 1, i];
+        array[array.GetLength(0) - 1, i] = tmp;
+    }
+}
+___________ 
+
+ЗАДАЧА 55:
+Задайте двумерный массив. Напишите программу, которая 
+заменяет строки на столбцы. В случае, если это невозможно, программа 
+должна вывести сообщение для пользователя.
+*/
+
+int[,] array = Generate2DArray(3, 3);
+
+Print2DArray(array);
+ChangeRowsArray(array);
+Console.WriteLine();
+Print2DArray(array);
+
+int[,] Generate2DArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j]);
+            Console.Write(" ");
+        }
+
+        Console.WriteLine();
+    }
+}
+/*
+void RevesreArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        int tmp = array[0, i];
+        array[0, i] = array[array.GetLength(0) - 1, i];
+        array[array.GetLength(0) - 1, i] = tmp;
+    }
+}
+*/ 
+void ChangeRowsArray(int[,] array)
+{
+/*array[0, 0]
+array[0, 0]
+array[0, 0]
+array[0, 0]
+
+array[0, 0]
+array[0, 0]
+array[0, 0]
+array[0, 0]
+*/
+
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        int tmp = array[0, i];
+        array[0, i] = array[array.GetLength(0) - 1, i];
+        array[array.GetLength(0) - 1, i] = tmp;
+    }
+}
